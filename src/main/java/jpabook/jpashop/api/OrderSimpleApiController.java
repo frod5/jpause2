905ed494f5085930c1d2caa486c3a845dabb9d3b
sunwooKim <frod5@kakao.com>
2023-05-17 18:00:14 +0900

    실전 스프링부트와 JPA 활용2 - 지연로딩과 조회 성능 최적화 V1 - 엔티티 직접 노출 (지연로딩 강제 초기화)

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,6 +24,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+//        for (Order order : all) { 지연로딩 강제 초기화
+//            order.getMember().getName();
+//            order.getDelivery().getAddress();
+//        }
         return all;
     }
 }
