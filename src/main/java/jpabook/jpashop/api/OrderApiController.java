3676d73730b81d94cc3dd5a567c7d642263f7ffa
sunwooKim <frod5@kakao.com>
2023-05-18 14:07:32 +0900

    실전 스프링부트와 JPA 활용2 - 컬렉션 조회 최적화 - V1 엔티티 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repositroy.OrderRepository;
+import jpabook.jpashop.repositroy.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        //지연로딩 초기
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+
+        return all;
+    }
+
+}
